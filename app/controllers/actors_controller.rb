483a0c23fc7dc2503a4535053f7ebe68d750bfd8
Jose Murillo <josemurillo2017@gmail.com>
2017-02-05 23:53:09 -0600

    Nearly done with actors
    Just need to add links. Actors very similar to directors. Copy

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -1,47 +1,55 @@
 class ActorsController < ApplicationController
   def index
     @model_list = Actor.all
-    render("/show.html.erb")
+    render("show.html.erb")
   end
 
   def find
     @id = params[:id]
-    @object = Actors.find(@id)
-    render("/find.html.erb")
+    @model = Actor.find(@id)
+    render("find.html.erb")
   end
 
   def delete
     @id = params[:id]
-    @object = Actors.find(@id)
+    @object = Actor.find(@id)
     @object.delete
-    redirect_to("/index.html.erb")
+    redirect_to("/actors")
   end
 
   def create_form
-    render("/create_form.html.erb")
+    render("create_form.html.erb")
   end
 
 
   def save
-    p = Actors.new
+    p = Actor.new
     #Insert specific info
+    p.name = params["the_name"]
+    p.bio = params["the_bio"]
+    p.dob = params["the_dob"]
+    p.image_url = params["the_image_url"]
     p.save
-    render("/save.html.erb")
+    redirect_to("/actors")
   end
 
   def edit_form
     @id = params[:id]
-    @object = Actors.find(@id)
-    render("/edit_form.html.erb")
+    @object = Actor.find(@id)
+    render("edit_form.html.erb")
   end
 
   def update
     @id = params[:id]
-    @object = Actors.find(@id)
+    p = Actor.find(@id)
     #Insert specific info
-
-
-    @object.save
-    render("/update.html.erb")
+    p.name = params["the_name"]
+    p.bio = params["the_bio"]
+    p.dob = params["the_dob"]
+    p.image_url = params["the_image_url"]
+    p.save
+    redirect_to("/actors")
   end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
+  get("/",{ :controller => "actors", :action => "index" } )
 
   #Update form
   get("/movies/:id/edit", { :controller => "movies", :action => "edit_form" })
@@ -10,10 +10,20 @@ Rails.application.routes.draw do
   get("/update_movie", { :controller => "movies", :action => "update" })
   get("/update_actor", { :controller => "actors", :action => "update" })
   get("/update_director", { :controller => "directors", :action => "update"})
-  
+
   get("/movies", { :controller => "movies", :action => "index" })
   get("/actors", { :controller => "actors", :action => "index" })
   get("/directors", { :controller => "directors", :action => "index" })
+
+  #Create specific person
+  get("/movies/new_form", { :controller => "movies", :action => "create_form" })
+  get("/actors/new_form", { :controller => "actors", :action => "create_form" })
+  get("/directors/new_form", { :controller => "directors", :action => "create_form"})
+
+  #Save specific person
+  get("/save_movie", { :controller => "movies", :action => "save" })
+  get("/save_actor", { :controller => "actors", :action => "save" })
+  get("/save_director", { :controller => "directors", :action => "save"})
 
   # Search specific person
   get("/movies/:id", { :controller => "movies", :action => "find" })
@@ -25,19 +35,6 @@ Rails.application.routes.draw do
   get("/delete_actor/:id", { :controller => "actors", :action => "delete" })
   get("/delete_director/:id", { :controller => "directors", :action => "delete" })
 
-  #Create specific person
-  get("/create_movie", { :controller => "movies", :action => "create_form" })
-  get("/create_actor", { :controller => "actors", :action => "create_form" })
-  get("/create_director", { :controller => "directors", :action => "create_form"})
-
-  #Save specific person
-  get("/save_movie", { :controller => "movies", :action => "save" })
-  get("/save_actor", { :controller => "actors", :action => "save" })
-  get("/save_director", { :controller => "directors", :action => "save"})
-
-
-
-  #Update
 
 
 end
